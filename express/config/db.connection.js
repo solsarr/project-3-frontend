a85f4a9159666c30201dee0200b6215b641116bb
Feven Seifu <fevenmenelik@Fevens-MacBook-Pro.local>
2023-01-03 14:22:30 -0500

    connect to mongodb

---
+++ express/config/db.connection.js
@@ -0,0 +1,16 @@
+// DEPENDENCIES
+
+// pull PORT from .env, give default value of 4000
+const mongoose = require('mongoose');
+const {MONGODB_URI} = process.env
+
+
+// DATABASE CONNECTION
+mongoose.set('strictQuery', true);
+mongoose.connect(MONGODB_URI)
+
+// Connection Events
+mongoose.connection
+  .on("open", () => console.log("You are connected to mongoose"))
+  .on("close", () => console.log("You are disconnected from mongoose"))
+  .on("error", (error) => console.log(error));
