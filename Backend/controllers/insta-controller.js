ac39530faa029523a0dc1eae75e698d219bb45a1
Feven Seifu <fevenmenelik@Fevens-MacBook-Pro.local>
2023-01-04 14:36:55 -0500

    create route and working

--- Backend/controllers/insta-controller.js
+++ Backend/controllers/insta-controller.js
@@ -2,14 +2,28 @@ const express = require('express')
 const router = express.Router()
 
 const {Profile} = require('../models')
-console.log(Profile)
+// console.log(Profile)
 // Routes
+// INDEX route
 router.get('/', (req,res)=>{
     res.status(200).json({message: "insta index route"})
 })
-
+// Create route
 router.post('/', (req,res)=>{
     res.status(200).json({message: "insta create/post route"})
+})
+
+// SHOW/GET route
+router.get('/:id', (req,res)=>{
+    res.status(200).json({message: "insta show/get route"})
+})
+// DELETE route
+router.delete('/:id', (req,res)=>{
+    res.status(200).json({message: "insta delete/destory route"})
+})
+// UPDATE/PUT route
+router.put('/:id', (req,res)=>{
+    res.status(200).json({message: "insta updatte/put route"})
 })
 
 module.exports = router
