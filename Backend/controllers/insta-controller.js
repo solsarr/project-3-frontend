5df98148fa373c0eaf6564105ecb4ef0630d657b
Feven Seifu <fevenmenelik@Fevens-MacBook-Pro.local>
2023-01-04 15:13:46 -0500

    Post route working

--- Backend/controllers/insta-controller.js
+++ Backend/controllers/insta-controller.js
@@ -1,17 +1,26 @@
 const express = require('express')
 const router = express.Router()
 
-const {Profile} = require('../models')
-// console.log(Profile)
+const {User} = require('../models')
+
+const db = require('../models')
+console.log(User)
 // Routes
 // INDEX route
 router.get('/', (req,res)=>{
     res.status(200).json({message: "insta index route"})
 })
 // Create route
-router.post('/', (req,res)=>{
-    // console.log('post route', req.body)
-    res.status(200).json({message: "insta create/post route"})
+router.post('/', async (req,res)=>{
+    console.log('post route', req.body)
+    try{
+        const newPost= await User.create(req.body)
+        res.status(201).json(newPost)
+
+    } catch(err){
+        res.status(400).json({error:err})
+    }
+    // res.status(200).json({message: "insta create/post route"})
 })
 
 // SHOW/GET route
