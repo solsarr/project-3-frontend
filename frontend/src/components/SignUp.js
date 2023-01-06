d116a391d6170c7bf1da5664fc266a023594c37f
Feven Seifu <fevenmenelik@Fevens-MacBook-Pro.local>
2023-01-06 01:40:32 -0500

    the change we made on manifest

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,121 +1,121 @@
 import React, { useRef } from "react";
-import {Link, Routes, Route, useNavigate} from 'react-router-dom';
+import { Link, Routes, Route, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import ProfilePage from "./Profile";
 
-export default function Form (props) {
+export default function Form(props) {
 
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
-// states for registration
-const [name, setName] = useState('');
-const [url, setURL] = useState('');
+  // states for registration
+  const [name, setName] = useState('');
+  const [url, setURL] = useState('');
 
 
-// states for checking the errors
-const [submitted, setSubmitted] = useState(false);
-const [error, setError] = useState(false);
+  // states for checking the errors
+  const [submitted, setSubmitted] = useState(false);
+  const [error, setError] = useState(false);
 
-// handling the name change
-const handleName = (e) => {
-setName(e.target.value);
-setSubmitted(false);
-};
+  // handling the name change
+  const handleName = (e) => {
+    setName(e.target.value);
+    setSubmitted(false);
+  };
 
-// handling the email change
-const handleURL = (e) => {
-setURL(e.target.value);
-setSubmitted(false);
-};
+  // handling the email change
+  const handleURL = (e) => {
+    setURL(e.target.value);
+    setSubmitted(false);
+  };
 
-// handling the password change
-// const handlePassword = (e) => {
-// setPassword(e.target.value);
-// setSubmitted(false);
-// };
+  // handling the password change
+  // const handlePassword = (e) => {
+  // setPassword(e.target.value);
+  // setSubmitted(false);
+  // };
 
-// handling the form submission
-const handleSubmit = (e) => {
-e.preventDefault();
-if (name === '' ) {
-setError(true);
-} else {
-setSubmitted(true);
-setError(false);
-fetch('http://localhost:4000/insta', {
-  method: "POST",
-  headers: {
-    'Content-type': 'application/json'
-  },
-  body: JSON.stringify(name)
-})
-.then((response) => response.json())
-.then((result) => {
-  console.log(result)
-});
-navigate('/profile');
+  // handling the form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name === '') {
+      setError(true);
+    } else {
+      setSubmitted(true);
+      setError(false);
+      fetch('http://localhost:4000/insta', {
+        method: "POST",
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(name)
+      })
+        .then((response) => response.json())
+        .then((result) => {
+          console.log(result)
+        });
+      navigate('/profile');
 
-}
-};
+    }
+  };
 
-// showing success message
-const successMessage = () => {
-return (
-<div
-className="success"
-style={{
-display: submitted ? '' : 'none',
-}}>
-<h1>User {name} successfully registered!!</h1>
-</div>
-);
-};
+  // showing success message
+  const successMessage = () => {
+    return (
+      <div
+        className="success"
+        style={{
+          display: submitted ? '' : 'none',
+        }}>
+        <h1>User {name} successfully registered!!</h1>
+      </div>
+    );
+  };
 
-// showing error message if error is true
-const errorMessage = () => {
-return (
-<div
-className="error"
-style={{
-display: error ? '' : 'none',
-}}>
-<h1>Please enter all the fields</h1>
-</div>
-);
-};
+  // showing error message if error is true
+  const errorMessage = () => {
+    return (
+      <div
+        className="error"
+        style={{
+          display: error ? '' : 'none',
+        }}>
+        <h1>Please enter all the fields</h1>
+      </div>
+    );
+  };
 
-return (
-<div className="form">
-<div>
-<h1>User Registration</h1>
-</div>
+  return (
+    <div className="form">
+      <div>
+        <h1>User Registration</h1>
+      </div>
 
-{/* Calling to the methods */}
-<div className="messages">
-{errorMessage()}
-{successMessage()}
-</div>
+      {/* Calling to the methods */}
+      <div className="messages">
+        {errorMessage()}
+        {successMessage()}
+      </div>
 
-<form>
-{/* Labels and inputs for form data */}
-<label className="label">Name</label>
-<input onChange={handleName} className="input"
-value={name} type="text" />
+      <form>
+        {/* Labels and inputs for form data */}
+        <label className="label">Name</label>
+        <input onChange={handleName} className="input"
+          value={name} type="text" />
 
-<label className="label">Profile Pic(URL)</label>
-<input onChange={handleURL} className="input"
-value={url} type="URL" />
+        <label className="label">Profile Pic(URL)</label>
+        <input onChange={handleURL} className="input"
+          value={url} type="URL" />
 
-{/* <label className="label">Password</label>
+        {/* <label className="label">Password</label>
 <input onChange={handlePassword} className="input"
 value={password} type="password" /> */}
 
-<button onClick={handleSubmit} className="btn" type="submit">
-Submit
-</button>
-</form>
-{/* {<ProfilePage name = {name}/>} */}
-</div>
+        <button onClick={handleSubmit} className="btn" type="submit">
+          Submit
+        </button>
+      </form>
+      {/* {<ProfilePage name = {name}/>} */}
+    </div>
 
-);
+  );
 }
